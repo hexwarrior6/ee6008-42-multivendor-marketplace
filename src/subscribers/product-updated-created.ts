f2cfae1d6ca5b5a8458a7f90cce77ac5f3b3c544
tanjiawei01@gmail.com <tanjiawei01@gmail.com>
2026-04-12 12:51:49 +0800

    Added subscriber to call storefront to revalidate product cache on product created and updated

---
+++ src/subscribers/product-updated-created.ts
@@ -0,0 +1,14 @@
+import type { SubscriberConfig, SubscriberArgs } from "@medusajs/medusa"
+
+type EventData = { id: string }
+
+export default async function productCreatedUpdatedHandler({
+    event: { data },
+    container,
+}: SubscriberArgs<EventData>) {
+    await fetch(`${process.env.STORE_CORS}/api/revalidate?tags=products`)
+}
+
+export const config: SubscriberConfig = {
+    event: ["product.created", "product.updated"],
+}
